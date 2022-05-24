5a0555a494ab770ae1093ded460730091626199b
unirefadm <uniref@inbox.ru>
2022-05-24 08:53:11 +0300

    init project

---
+++ src/main/java/org/uniref/UnirefApplication.java
@@ -0,0 +1,9 @@
+package org.uniref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnirefApplication {
+    public static void main(String[] args) { SpringApplication.run(UnirefApplication.class, args); }
+}
