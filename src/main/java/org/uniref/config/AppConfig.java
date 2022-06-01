3b047ff70d6b78644d8c626a84cf5c6c9c211d58
unirefadm <uniref@inbox.ru>
2022-06-01 09:31:10 +0300

    [+] add data-type table migration

---
+++ src/main/java/org/uniref/config/AppConfig.java
@@ -0,0 +1,9 @@
+package org.uniref.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"org.uniref"})
+class AppConfig {
+}
